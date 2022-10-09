206e947b2e350017a4f2cd1e38332060b9b0d679
al-shaimon <alshaimon968@gmail.com>
2022-10-10 00:52:43 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import CardGroup from './components/CardGroup/CardGroup';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="spinner-border text-danger" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <CardGroup></CardGroup>
+    </div>
+  );
+}
+
+export default App;
